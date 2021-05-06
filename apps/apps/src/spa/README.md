99a46b486fcaaec3663f817924223c49f6c538a4
Dayne <dayne@code.org>
2021-05-06 15:40:04 -0700

    Adding basic webpack config with hello world javascript

---
+++ apps/apps/src/spa/README.md
@@ -0,0 +1,7 @@
+## Dev setup
+Run the following commands from the `code-dot-org/apps` directory.
+### Automatically compile webpack files
+```yarn webpack --config webpack-spa.config.js --watch```
+
+### Run SPA http server
+```yarn http-server ./dist/spa```
---
+++ apps/webpack-spa.config.js
@@ -0,0 +1,15 @@
+// Generated using webpack-cli http://github.com/webpack-cli
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/spa/spa.js',
+  output: {
+    path: path.resolve(__dirname, 'dist/spa'),
+    filename: 'main.js'
+  },
+  plugins: [],
+  module: {
+    rules: []
+  }
+};
