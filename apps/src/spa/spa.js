5e5400f2dd640bc4e446b96451e57d790d220e50
Anna Xu <annaxu@wustl.edu>
2021-05-13 12:00:36 -0700

    experimenting with adding react code

--- apps/src/spa/spa.js
+++ apps/src/spa/spa.js
@@ -1,2 +1,10 @@
 require('file-loader?name=[name].[ext]!./index.html');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+var SPAReact = require('./spa.jsx');
+
 console.log('Hello Dayne and Anna');
+
+// console.log(SPAReact);
+ReactDom.render(<SPAReact />);
